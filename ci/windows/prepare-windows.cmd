ad2ecc9fd05162b6093b6b0b221d0354d7bf0b92
Norihiro Kamae <norihiro@nagater.net>
2020-11-28 22:25:59 +0900

    ci(windows): clean LIBVNCCLIENT_LIBRARIES

--- ci/windows/prepare-windows.cmd
+++ ci/windows/prepare-windows.cmd
@@ -10,12 +10,7 @@ cmake -G "Visual Studio 16 2019" ^
 -DOBS_FRONTEND_LIB="%OBSPath%\build32\UI\obs-frontend-api\%build_config%\obs-frontend-api.lib" ^
 -DLIBVNCCLIENT_INCLUDE_DIRS="%LIBVNCPath32%" ^
 -DLIBVNCCLIENT_LIB_DIRS="%LIBVNCPath32%" ^
--DLIBVNCCLIENT_LIBRARIES="%LIBVNCPath32%\%build_config%\vncclient.lib" ^
--DPTHREAD_LIBRARIES="%OBSPath%\build32\deps\w32-pthreads\%build_config%\w32-pthreads.lib" ^
--DZLIB_LIB="%LIBVNCPath32%\deps\zlib\%build_config%\zlibstatic.lib" ^
--DLZO_LIB="%LIBVNCPath32%\deps\lzo\build\%build_config%\lzo2.lib" ^
--DLIBPNG_LIB="%LIBVNCPath32%\deps\libpng\%build_config%\libpng16_static.lib" ^
--DLIBJPEG_LIB="%LIBVNCPath32%\deps\libjpeg\%build_config%\turbojpeg-static.lib" ^
+-DLIBVNCCLIENT_LIBRARIES="%LIBVNCPath32%\%build_config%\vncclient.lib;%OBSPath%\build32\deps\w32-pthreads\%build_config%\w32-pthreads.lib;%LIBVNCPath32%\deps\zlib\%build_config%\zlibstatic.lib;%LIBVNCPath32%\deps\lzo\build\%build_config%\lzo2.lib;%LIBVNCPath32%\deps\libpng\%build_config%\libpng16_static.lib;%LIBVNCPath32%\deps\libjpeg\%build_config%\turbojpeg-static.lib;wsock32.lib;ws2_32.lib" ^
 ..
 cd ..
 :skippedWin32
@@ -31,12 +26,7 @@ cmake -G "Visual Studio 16 2019" ^
 -DOBS_FRONTEND_LIB="%OBSPath%\build64\UI\obs-frontend-api\%build_config%\obs-frontend-api.lib" ^
 -DLIBVNCCLIENT_INCLUDE_DIRS="%LIBVNCPath64%" ^
 -DLIBVNCCLIENT_LIB_DIRS="%LIBVNCPath64%" ^
--DLIBVNCCLIENT_LIBRARIES="%LIBVNCPath64%\%build_config%\vncclient.lib" ^
--DPTHREAD_LIBRARIES="%OBSPath%\build64\deps\w32-pthreads\%build_config%\w32-pthreads.lib" ^
--DZLIB_LIB="%LIBVNCPath64%\deps\zlib\%build_config%\zlibstatic.lib" ^
--DLZO_LIB="%LIBVNCPath64%\deps\lzo\build\%build_config%\lzo2.lib" ^
--DLIBPNG_LIB="%LIBVNCPath64%\deps\libpng\%build_config%\libpng16_static.lib" ^
--DLIBJPEG_LIB="%LIBVNCPath64%\deps\libjpeg\%build_config%\turbojpeg-static.lib" ^
+-DLIBVNCCLIENT_LIBRARIES="%LIBVNCPath64%\%build_config%\vncclient.lib;%OBSPath%\build64\deps\w32-pthreads\%build_config%\w32-pthreads.lib;%LIBVNCPath64%\deps\zlib\%build_config%\zlibstatic.lib;%LIBVNCPath64%\deps\lzo\build\%build_config%\lzo2.lib;%LIBVNCPath64%\deps\libpng\%build_config%\libpng16_static.lib;%LIBVNCPath64%\deps\libjpeg\%build_config%\turbojpeg-static.lib;wsock32.lib;ws2_32.lib" ^
 ..
 
 REM Import the generated includes to get the plugin's name
