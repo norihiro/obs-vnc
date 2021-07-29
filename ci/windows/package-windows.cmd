75fe8fd6e00e58571564de8e6ddac5608f331d08
Norihiro Kamae <norihiro@nagater.net>
2021-07-30 02:26:46 +0900

    ci: copy license files for bundled libraries
    License files for jpeg, libpng, and liblzo are added to the package.

--- ci/windows/package-windows.cmd
+++ ci/windows/package-windows.cmd
@@ -8,6 +8,9 @@ set /p PackageVersion=<package-version.txt
 del package-version.txt
 
 copy ..\LICENSE          ..\release\data\obs-plugins\%PluginName%\LICENCE-%PluginName%.txt
+copy %LIBVNCPath64%\deps\libjpeg\LICENSE.md ..\release\data\obs-plugins\%PluginName%\LICENSE-libjpeg.md
+copy %LIBVNCPath64%\deps\libpng\LICENSE ..\release\data\obs-plugins\%PluginName%\LICENSE-libpng.txt
+copy %LIBVNCPath64%\deps\lzo\COPYING ..\release\data\obs-plugins\%PluginName%\COPYING-lzo.txt
 
 REM Package ZIP archive
 7z a "%PluginName%-%PackageVersion%-Windows.zip" "..\release\*"
