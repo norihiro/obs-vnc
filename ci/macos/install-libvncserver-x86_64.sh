c5d2f07f51633388ff36f10db58fb53009656a87
Norihiro Kamae <norihiro@nagater.net>
2022-07-26 22:25:20 +0900

    CI: Rewrite for OBS 28
    This commit also starts to use zlib from obs-deps on Windows.

---
+++ ci/macos/install-libvncserver-x86_64.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+set -ex
+
+brew install pkg-config
+
+brew install jpeg-turbo libpng lzo openssl libvncserver
+cp /usr/local/opt/jpeg-turbo/LICENSE.md data/LICENSE-jpeg
+cp /usr/local/opt/libpng/LICENSE data/LICENSE-libpng
+cp /usr/local/opt/lzo/COPYING data/COPYING-lzo
+cp /usr/local/opt/libvncserver/COPYING data/COPYING-libvncserver
+
+sudo ed /usr/local/lib/pkgconfig/libvncclient.pc <<-EOF
+/^Libs:/
+.s;Libs: ;Libs: -L/usr/local/lib -L/usr/local/opt/jpeg-turbo/lib -L/usr/local/opt/openssl@1.1/lib ;
+wq
+EOF
--- ci/windows/package-windows.cmd
+++ ci/windows/package-windows.cmd
@@ -8,12 +8,15 @@ set /p PackageVersion=<package-version.txt
 del package-version.txt
 
 copy ..\LICENSE          ..\release\data\obs-plugins\%PluginName%\LICENCE-%PluginName%.txt
-copy %LIBVNCPath64%\deps\libjpeg\LICENSE.md ..\release\data\obs-plugins\%PluginName%\LICENSE-libjpeg.md
-copy %LIBVNCPath64%\deps\libpng\LICENSE ..\release\data\obs-plugins\%PluginName%\LICENSE-libpng.txt
-copy %LIBVNCPath64%\deps\lzo\COPYING ..\release\data\obs-plugins\%PluginName%\COPYING-lzo.txt
+copy %LIBVNCPath%\deps\libjpeg\LICENSE.md ..\release\data\obs-plugins\%PluginName%\LICENSE-libjpeg.md
+copy %LIBVNCPath%\deps\libpng\LICENSE ..\release\data\obs-plugins\%PluginName%\LICENSE-libpng.txt
+copy %LIBVNCPath%\deps\lzo\COPYING ..\release\data\obs-plugins\%PluginName%\COPYING-lzo.txt
 
 REM Package ZIP archive
-7z a "%PluginName%-%PackageVersion%-Windows.zip" "..\release\*"
+7z a "%PluginName%-%PackageVersion%-obs%1-Windows.zip" "..\release\*"
 
 REM Build installer
-iscc ..\installer\installer-Windows.generated.iss /O. /F"%PluginName%-%PackageVersion%-Windows-Installer"
+iscc ..\installer\installer-Windows.generated.iss /O. /F"%PluginName%-%PackageVersion%-obs%1-Windows-Installer"
+
+certutil.exe -hashfile "%PluginName%-%PackageVersion%-obs%1-Windows.zip" SHA1
+certutil.exe -hashfile "%PluginName%-%PackageVersion%-obs%1-Windows-Installer.exe" SHA1
