c5fa98a8e824671935c1699c44d9e42bec139f97
Norihiro Kamae <norihiro@nagater.net>
2023-06-10 14:44:17 +0900

    fixup! [WIP] .github (macOS): Build libvncserver

--- ci/macos/build-libvncserver.sh
+++ ci/macos/build-libvncserver.sh
@@ -13,7 +13,7 @@ tar -xzf deps/lzo.tar.gz -C deps/lzo/ --strip-components 1
 pushd deps/lzo
 cmake '-DCMAKE_OSX_ARCHITECTURES=x86_64;arm64' -B build
 cmake --build build
-cmake --install build
+sudo cmake --install build
 popd
 file deps/lzo/build/liblzo2.a
 cat deps/lzo/build/lzo2.pc
@@ -25,7 +25,7 @@ tar -xzf deps/libjpeg.tar.gz -C deps/libjpeg --strip-components 1
 pushd deps/libjpeg
 cmake '-DCMAKE_OSX_ARCHITECTURES=x86_64;arm64' -B build -DENABLE_SHARED=OFF
 cmake --build build
-cmake --install build
+sudo cmake --install build
 popd
 file deps/libjpeg/build/lib*jpeg.a
 
@@ -40,9 +40,42 @@ cmake '-DCMAKE_OSX_ARCHITECTURES=x86_64;arm64' -B build \
 	-DCMAKE_ASM_FLAGS='-DPNG_ARM_NEON_IMPLEMENTATION=1' -DPNG_ARM_NEON=on
 mkdir -p build/arm64
 cmake --build build
-cmake --install build
+sudo cmake --install build
 popd
 file deps/libpng/build/libpng16.a
+
+##############################################################################################################
+
+curl -L -o deps/gnutls.tar.xz https://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/gnutls-3.6.16.tar.xz
+mkdir deps/gnutls
+tar xJf deps/gnutls.tar.xz -C deps/gnutls --strip-components 1
+
+for a in arm64 x86_64; do
+	mkdir deps/gnutls/build-$a
+	pushd deps/gnutls/build-$a
+	case "$a" in
+		arm64)
+			config_opts=(
+				--host aarch64-apple-darwin20
+				--prefix=/tmp/deps/arm64
+				CFLAGS=-I/tmp/deps/arm64/include
+				LDFLAGS=-L/tmp/deps/arm64/lib
+			)
+			;;
+		x86_64)
+			config_opts=()
+			;;
+	esac
+	PKG_CONFIG_PATH=/tmp/deps/$a/lib/pkgconfig: \
+		../configure "${config_opts[@]}" \
+		--disable-shared \
+		--enable-static
+	make -j4
+	popd
+done
+
+
+##############################################################################################################
 
 git clone https://github.com/LibVNC/libvncserver.git deps/libvncserver
 pushd deps/libvncserver
@@ -56,3 +89,5 @@ cmake \
 	-DWITH_EXAMPLES=OFF \
 	-B build
 cmake --build . --config $build_config
+
+test -f "$GITHUB_OUTPUT" && echo 'success=true' >> $GITHUB_OUTPUT
